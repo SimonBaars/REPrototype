72ae488a5748f4dd95f14ffe5446ce5b8eb8d23d
Simon <SJ.Baars@student.han.nl>
2018-10-16 12:55:45 +0200

    Created a new prototype

---
+++ canvas/eval/src/resource.js
@@ -0,0 +1,35 @@
+var s_HelloWorld = "res/HelloWorld.png";
+var s_CloseNormal = "res/CloseNormal.png";
+var s_CloseSelected = "res/CloseSelected.png";
+var heads = [0,1,2,3,4,5,6,7,8,9,10];
+var g_ressources = [
+    //image
+    {type:"image", src:"res/Icon.png"},
+    {type:"image", src:"res/head0.png"},
+    {type:"image", src:"res/head1.png"},
+    {type:"image", src:"res/head2.png"},
+    {type:"image", src:"res/head3.png"},
+    {type:"image", src:"res/head4.png"},
+    {type:"image", src:"res/head5.png"},
+    {type:"image", src:"res/head6.png"},
+    {type:"image", src:"res/head7.png"},
+    {type:"image", src:"res/head8.png"},
+    {type:"image", src:"res/head9.png"},
+    {type:"image", src:"res/head10.png"},
+    {type:"image", src:"res/leftarm.png"},
+    {type:"image", src:"res/rightarm.png"},
+    {type:"image", src:"res/body.png"},
+    {type:"image", src:"res/leftleg.png"},
+    {type:"image", src:"res/rightleg.png"}
+
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
